6724c8b679f7e092bd91291cd5db174d8ad837f0
sandoschwert <sandi.schwert@gmail.com>
2017-03-07 21:03:28 -0600

    adding project structure with class and method skeletons

---
+++ MeasurementConversionAPI/src/main/java/edu/matc/Convert.java
@@ -0,0 +1,19 @@
+package edu.matc;
+
+/**
+ * Created by Sandi Schwert on 3/7/17.
+ */
+public class Convert {
+
+    Math newMath;
+    Units newUnits;
+    double userQuantityInput;
+    String userUnitsInput;
+
+    //Conversion Methods
+    //getUserQuantityInput()
+    //getUserUnitsInput()
+    //calculateConversion()
+    //returnResponse()
+
+}
