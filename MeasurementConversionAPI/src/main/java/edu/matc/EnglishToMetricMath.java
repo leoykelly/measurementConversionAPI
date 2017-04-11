2df335318f0f708c4e4398c585bcdac76b25444b
sandoschwert <sandi.schwert@gmail.com>
2017-04-10 19:06:04 -0500

    adding rest of Length Measurement math

--- MeasurementConversionAPI/src/main/java/edu/matc/EnglishToMetricMath.java
+++ MeasurementConversionAPI/src/main/java/edu/matc/EnglishToMetricMath.java
@@ -5,6 +5,8 @@ package edu.matc;
  */
 public class EnglishToMetricMath {
 
+    /*EnglishToMetricConversions*/
+
     public double convertInchesToMillimeters(double length) {
         double convertedLength = length * 25.4;
         return convertedLength;
@@ -19,12 +21,6 @@ public class EnglishToMetricMath {
         double convertedLength = length * .0254;
         return convertedLength;
     }
-
-    /* THIS IS A USELESS FORMULA DON'T BOTHER WITH KILOMETERS UNLESS BACKWARDS
-       public double convertInchesToKilometers(double length) {
-        double convertedLength = length * .00000254;
-        return convertedLength;
-    }*/
 
     public double convertFeetToMillimeters(double length) {
         double convertedLength = length * 304.8;
@@ -42,18 +38,67 @@ public class EnglishToMetricMath {
     }
 
     public double convertYardsToMillimeters(double length) {
-        double convertedLength = length * 30.48;
+        double convertedLength = length * 914.4;
         return convertedLength;
     }
 
     public double convertYardsToCentimeters(double length) {
-        double convertedLength = length * 30.48;
+        double convertedLength = length * 91.44;
         return convertedLength;
     }
 
     public double convertYardsToMeters(double length) {
-        double convertedLength = length * 30.48;
+        double convertedLength = length * .9144;
         return convertedLength;
     }
+
+    /* Metric to English Conversions*/
+
+    public double convertMillimeterToInch (double length) {
+        double convertedLength = length * 0.03937;
+        return convertedLength;
+    }
+
+    public double convertMillimeterToFoot (double length) {
+        double convertedLength = length * 0.00328084;
+        return convertedLength;
+    }
+
+    public double convertMillimeterToYard (double length) {
+        double convertedLength = length * 0.00109361;
+        return convertedLength;
+    }
+
+    public double convertCentimeterToInch (double length) {
+        double convertedLength = length * 0.393701;
+        return convertedLength;
+    }
+
+    public double convertCentimeterToFoot (double length) {
+        double convertedLength = length * 0.0328084;
+        return convertedLength;
+    }
+
+    public double convertCentimeterToYard (double length) {
+        double convertedLength = length * 0.0109361;
+        return convertedLength;
+    }
+
+    public double convertMeterToInch (double length) {
+        double convertedLength = length * 39.3701;
+        return convertedLength;
+    }
+
+    public double convertMeterToFoot (double length) {
+        double convertedLength = length * 3.28084;
+        return convertedLength;
+    }
+
+    public double convertMeterToYard (double length) {
+        double convertedLength = length * 1.09361;
+        return convertedLength;
+    }
+
+    /* Miles and Kilometers ???????? */
 
 }
