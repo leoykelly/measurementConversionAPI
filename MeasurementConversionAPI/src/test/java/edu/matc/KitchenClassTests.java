27ba30e14110099f3fc3b9d43dbf9579e52bea9c
sandoschwert <sandi.schwert@gmail.com>
2017-04-13 07:20:46 -0500

    adding more tests

--- MeasurementConversionAPI/src/test/java/edu/matc/KitchenClassTests.java
+++ MeasurementConversionAPI/src/test/java/edu/matc/KitchenClassTests.java
@@ -1,7 +1,38 @@
 package edu.matc;
 
+import org.junit.*;
+import org.apache.log4j.Logger;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by sandischwert on 4/12/17.
  */
 public class KitchenClassTests {
+    KitchenMath kitchenMath = new KitchenMath();
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Before
+    double quantity;
+    double expectedResult;
+    double convertedQuantity;
+
+
+    @Test
+    public void testTspToTbsMath() throws Exception {
+        //kitchenMath = new KitchenMath();
+        quantity = 9;
+        expectedResult = 3;
+        convertedQuantity = kitchenMath.convertTspToTbs(quantity);
+
+        assertEquals("6 tsp is 2 tbs", expectedResult, convertedQuantity, .0001);
+    }
+
+    @Test
+    public void testTspToCupMath() throws Exception {
+
+
+
+    }
+
 }
